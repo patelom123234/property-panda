7ce523a672c1672e292659aec442f97962561f83
om patel <ompatel5044@gmail.com>
2024-04-04 16:44:07 +0530

    change in vite confige add rollup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react"; // Import the correct plugin for React
-
-// Import your Webpack configuration if needed
-import webpackConfig from "./webpack.config.js";
+import commonjs from "@rollup/plugin-commonjs"; // Import the Rollup CommonJS plugin
 
 export default defineConfig({
   server: {
@@ -10,12 +8,12 @@ export default defineConfig({
       ".scss": "text/x-scss",
     },
   },
-  // Use Webpack configuration if needed
-  configureWebpack: webpackConfig,
-  plugins: [react()], // Ensure the correct import of the react plugin
+  plugins: [
+    react(), // Ensure the correct import of the react plugin
+    commonjs(), // Add the Rollup CommonJS plugin
+  ],
   resolve: {
     alias: {
-      // Construct the absolute path based on the current module's URL
       "@": new URL("./src", import.meta.url).pathname,
     },
     extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
